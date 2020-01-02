5c6030280a44f34b7a2899a371582feadabf3a4a
Louis Popi <theh2o64@gmail.com>
2020-01-02 17:18:05 +0100

    net: ipc_router_core: Mark 277 and 256 as non-service

--- net/ipc_router/ipc_router_core.c
+++ net/ipc_router/ipc_router_core.c
@@ -239,6 +239,10 @@ static int is_sensor_port(struct msm_ipc_router_remote_port *rport)
 
 	if (rport && rport->server) {
 		svcid = rport->server->name.service;
+
+		if (svcid == 277 || svcid == 256)
+			return false;
+
 		if (svcid == 400 || (svcid >= 256 && svcid <= 320))
 			return true;
 	}
